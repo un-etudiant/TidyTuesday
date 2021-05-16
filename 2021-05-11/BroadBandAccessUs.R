689433227396b8be7f04a1c4de1300da41eb263b
Pradeep Sundaram <pradeepssundaram@gmail.com>
2021-05-16 20:44:05 +0530

    Added Final Changes

--- 2021-05-11/BroadBandAccessUs.R
+++ 2021-05-11/BroadBandAccessUs.R
@@ -4,6 +4,11 @@ library(janitor)
 library(zipcodeR)
 library(maps)
 library(ggthemes)
+library(tigris)
+library(ggrepel)
+
+options(tigris_use_cache = TRUE)
+
 ## Get data
 tuesdata <- tidytuesdayR::tt_load('2021-05-11')
 broadband <- tuesdata$broadband
@@ -11,47 +16,59 @@ broadband_zip <- tuesdata$broadband_zip
 rm(tuesdata)
 broadband <- broadband %>% janitor::clean_names() 
 broadband_zip <- broadband_zip %>%
-  janitor::clean_names() 
-
-# %>% 
-#   mutate(postal_code = as.character(postal_code)) %>% 
-#   rename(zipcode = postal_code) %>% 
-#   mutate(zipcode = normalize_zip(zipcode)) 
+  janitor::clean_names() %>% 
+  mutate(postal_code = as.character(postal_code)) %>%
+  rename(zipcode = postal_code) %>%
+  mutate(zipcode = normalize_zip(zipcode))
 
 
+zip_codes_info <- as_tibble(zip_code_db) %>% 
+select(zipcode,zipcode_type,county,state,population,major_city, mc_lat = lat,mc_long = lng) %>% 
+  filter(state == "WA") %>%  
+  filter(str_detect(county, "King")) %>% 
+  filter(!is.na(population)) %>% 
+  filter(zipcode_type == "Standard")
 
-##
-# zip_codes_info <- as_tibble(zip_code_db) %>% 
-#   select(zipcode,zipcode_type,county,state,lat,lng,population,population_density,median_home_value,median_household_income,bounds_west,bounds_east,bounds_north,bounds_south)
-# 
-# broadband_zip <- broadband_zip %>%
-#   left_join(zip_codes_info,by = c("zipcode" = "zipcode"))
-# 
-# broadband_zip <- broadband_zip %>%
-#   filter(zipcode_type == "Standard")
+
+broadband_zip_kingcounty <- broadband_zip %>% 
+  filter(st == "WA", county_name == "King" )
   
-broadband_zip_wa <- broadband_zip %>% filter(st == "WA") %>% 
-  mutate(broadband_usage = broadband_usage * 100) %>% 
-  group_by(county_name) %>% 
-  summarise(average_bb_usage = mean(broadband_usage)) %>% 
-  ungroup() %>% 
-  mutate(county_name = str_to_lower(county_name))
-wa_state_map <- map_data("county") %>% filter(region == "washington")
-
-broadband_zip_wa <- broadband_zip_wa %>% 
-  left_join(wa_state_map,by = c("county_name" = "subregion" ))
-
-ggplot(data = broadband_zip_wa) +
-  geom_polygon(mapping = aes(x= long , y= lat , group = group , fill = average_bb_usage ), color = "lightgrey") + 
-  theme_map()
-
-  
+broadband_zip_kingcounty <- broadband_zip_kingcounty %>% 
+  inner_join(zip_codes_info, by = "zipcode")
 
 
+kingcountyzips <- zipcodeR::search_county("king","wa") %>% 
+  filter(!is.na(population)) %>% 
+  filter(zipcode_type == "Standard") %>% 
+  distinct(zipcode) %>% pull()
+
+zipcode_shapefile <- tigris::zctas(cb = TRUE , starts_with = kingcountyzips)
 
 
-  
-  
-  
+zipcode_sf <- zipcode_shapefile %>% select(zipcode = ZCTA5CE10 , geometry)
+
+zipcode_sf <- zipcode_sf %>% 
+  inner_join(broadband_zip_kingcounty , by = "zipcode" )
+
+water_kc_sf <- area_water("wa","king")
+
+water_kc_sf <- water_kc_sf %>% 
+  filter(MTFCC %in% c("H2030","H2051","H2053")) %>% 
+  filter(str_detect(FULLNAME,"Washington") | str_detect(FULLNAME,"Sammamish")  | str_detect(FULLNAME , "Puget Sound")) 
+
+
+(broadbandusage <- ggplot(data = zipcode_sf) +
+  geom_sf( mapping = aes(fill = broadband_usage * 100), alpha = 0.6) +
+  scale_fill_distiller(direction = 1 ,type = "seq" , palette = "YlGn") +
+  geom_sf(data = water_kc_sf, fill = "lightblue"  ) +
+  labs(fill = "Broadband Usage" , title = "Broadband Usage in King County by Zip Code, WA" , subtitle = "Does usage diminish the farther east we go ?" , caption = "Data By Microsoft Via The Verge | Visualization By @2rabbits2") +
+  theme_minimal() +
+  theme(legend.box = "horizontal" , legend.direction = "horizontal" , legend.position = "bottom")  +
+  theme(axis.line = element_blank() , axis.text = element_blank() , panel.grid = element_blank()) +
+  guides(fill = guide_colourbar(title.position = "top")) 
+)
+
+ggsave(filename = "./2021-05-11/broadbandusage.png",width = 15,
+       height = 9)
   
 
